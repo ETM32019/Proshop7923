dddd0ad3ac1cdf7df9d53470b6267f62944f444d
Landon McKell <landon.roney7923@gmail.com>
2020-12-01 19:58:47 -0700

    redux store, product list reducer & action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,37 @@
 import React, { useState, useEffect } from "react";
 import Product from "../components/Product";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+
+  const productList = useSelector(state => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
+    dispatch(listProducts());
+  }, [dispatch]);
 
-    fetchProducts();
-  }, []);
   return (
     <>
       <h1>Latest Products</h1>
-      <div className="row">
-        {products.map(product => (
-          <div
-            className="col-sm-12 col-md-6 col-lg-4 col-xl-3"
-            key={product._id}
-          >
-            <Product product={product} />
-          </div>
-        ))}
-      </div>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <div className="row">
+          {products.map(product => (
+            <div
+              className="col-sm-12 col-md-6 col-lg-4 col-xl-3"
+              key={product._id}
+            >
+              <Product product={product} />
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 };
