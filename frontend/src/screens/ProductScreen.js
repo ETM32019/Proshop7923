1d93ad66be604229f00ddf6e4f207e59fa6e5af2
Landon McKell <landon.roney7923@gmail.com>
2020-12-23 10:03:06 -0700

    morgan & create review endpoint

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,16 +2,33 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Rating from "../components/Rating";
 import { useDispatch, useSelector } from "react-redux";
-import { listProductDetails } from "../actions/productActions";
+import {
+  listProductDetails,
+  createProductReview
+} from "../actions/productActions";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
+import { PRODUCT_CREATE_REVIEW_RESET } from "../constants/productConstants";
 
 const ProductScreen = ({ match, history }) => {
   const [qty, setQty] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
+
   const dispatch = useDispatch();
 
   const productDetails = useSelector(state => state.productDetails);
   const { loading, error, product } = productDetails;
+
+  const userLogin = useSelector(state => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const productReviewCreate = useSelector(state => state.productReviewCreate);
+  const {
+    error: errorProductReview,
+    loading: loadingProductReview,
+    success: successProductReview
+  } = productReviewCreate;
 
   useEffect(() => {
     dispatch(listProductDetails(match.params.id));
@@ -19,6 +36,16 @@ const ProductScreen = ({ match, history }) => {
 
   const addToCartHandler = () => {
     history.push(`/cart/${match.params.id}?qty=${qty}`);
+  };
+
+  const submitHandler = e => {
+    e.preventDefault();
+    dispatch(
+      createProductReview(match.params.id, {
+        rating,
+        comment
+      })
+    );
   };
 
   return (
@@ -31,84 +58,162 @@ const ProductScreen = ({ match, history }) => {
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
-        <div className="row">
-          <div className="col-md-6">
-            <img src={product.image} alt={product.name} className="img-fluid" />
-          </div>
-          <div className="col-md-3">
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">
-                <h3>{product.name}</h3>
-              </li>
-              <li className="list-group-item">
-                <Rating
-                  value={product.rating}
-                  text={`${product.numReviews} reviews`}
-                />
-              </li>
-              <li className="list-group-item">Price: ${product.price}</li>
-              <li className="list-group-item">
-                Description: {product.description}
-              </li>
-            </ul>
-          </div>
-          <div className="col-md-3">
-            <div className="card">
+        <>
+          <div className="row">
+            <div className="col-md-6">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="img-fluid"
+              />
+            </div>
+            <div className="col-md-3">
               <ul className="list-group list-group-flush">
                 <li className="list-group-item">
-                  <div className="row">
-                    <div className="col">Price:</div>
-                    <div className="col">
-                      <strong>${product.price}</strong>
-                    </div>
-                  </div>
+                  <h3>{product.name}</h3>
                 </li>
                 <li className="list-group-item">
-                  <div className="row">
-                    <div className="col">Status:</div>
-                    <div className="col">
-                      <strong>
-                        {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
-                      </strong>
-                    </div>
-                  </div>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                  />
                 </li>
-                {product.countInStock > 0 && (
+                <li className="list-group-item">Price: ${product.price}</li>
+                <li className="list-group-item">
+                  Description: {product.description}
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-3">
+              <div className="card">
+                <ul className="list-group list-group-flush">
                   <li className="list-group-item">
                     <div className="row">
-                      <div className="col">Qty</div>
+                      <div className="col">Price:</div>
                       <div className="col">
-                        <div className="form-group">
-                          <select
-                            className="form-control"
-                            value={qty}
-                            onChange={e => setQty(e.target.value)}
-                          >
-                            {[...Array(product.countInStock).keys()].map(x => (
-                              <option key={x + 1} value={x + 1}>
-                                {x + 1}
-                              </option>
-                            ))}
-                          </select>
-                        </div>
+                        <strong>${product.price}</strong>
                       </div>
                     </div>
                   </li>
-                )}
+                  <li className="list-group-item">
+                    <div className="row">
+                      <div className="col">Status:</div>
+                      <div className="col">
+                        <strong>
+                          {product.countInStock > 0
+                            ? "In Stock"
+                            : "Out of Stock"}
+                        </strong>
+                      </div>
+                    </div>
+                  </li>
+                  {product.countInStock > 0 && (
+                    <li className="list-group-item">
+                      <div className="row">
+                        <div className="col">Qty</div>
+                        <div className="col">
+                          <div className="form-group">
+                            <select
+                              className="form-control"
+                              value={qty}
+                              onChange={e => setQty(e.target.value)}
+                            >
+                              {[...Array(product.countInStock).keys()].map(
+                                x => (
+                                  <option key={x + 1} value={x + 1}>
+                                    {x + 1}
+                                  </option>
+                                )
+                              )}
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  )}
+                  <li className="list-group-item">
+                    <button
+                      type="button"
+                      onClick={addToCartHandler}
+                      className="btn btn-primary btn-lg btn-block"
+                      disabled={product.countInStock === 0}
+                    >
+                      Add to Cart
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>Review</h2>
+              {product.reviews.length === 0 && <Message>No Reviews</Message>}
+              <ul className="list-group list-group-flush">
+                {product.reviews.map(review => (
+                  <li className="list-group-item" key={review._id}>
+                    <strong>{review.name}</strong>
+                    <Rating value={review.rating} />
+                    <p>{review.createAt.substring(0, 10)}</p>
+                    <p>{review.comment}</p>
+                  </li>
+                ))}
                 <li className="list-group-item">
-                  <button
-                    type="button"
-                    onClick={addToCartHandler}
-                    className="btn btn-primary btn-lg btn-block"
-                    disabled={product.countInStock === 0}
-                  >
-                    Add to Cart
-                  </button>
+                  <h2>Write a Customer Review</h2>
+                  {successProductReview && (
+                    <Message variant="success">
+                      Review submitted successfully
+                    </Message>
+                  )}
+                  {loadingProductReview && <Loader />}
+                  {errorProductReview && (
+                    <Message variant="danger">{errorProductReview}</Message>
+                  )}
+                  {userInfo ? (
+                    <form onSubmit={submitHandler}>
+                      <label htmlFor="ratingSelect">Rating</label>
+                      <div className="input-group mb-3">
+                        <select
+                          className="custom-select"
+                          value={rating}
+                          onChange={e => setRating(e.target.value)}
+                        >
+                          <option selected>Select...</option>
+                          <option value="1">1 - Poor</option>
+                          <option value="2">2 - Fair</option>
+                          <option value="3">3 - Good</option>
+                          <option value="4">4 - Very Good</option>
+                          <option value="5">5 - Excellent</option>
+                        </select>
+                      </div>
+                      <div className="input-group">
+                        <label htmlFor="commentTextField">Comment</label>
+                        <textarea
+                          className="form-control"
+                          aria-label="With textarea"
+                          row="3"
+                          value={comment}
+                          onChange={e => setComment(e.target.value)}
+                        ></textarea>
+                      </div>
+                      <button
+                        disabled={loadingProductReview}
+                        type="submit"
+                        className="btn btn-primary"
+                      >
+                        Submit
+                      </button>
+                    </form>
+                  ) : (
+                    <Message>
+                      Please <Link to="/login">sign in</Link> to write a review{" "}
+                    </Message>
+                  )}
                 </li>
               </ul>
             </div>
           </div>
-        </div>
+        </>
       )}
     </>
   );
